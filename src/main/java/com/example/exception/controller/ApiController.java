839e328d095c089568bb9f36ae0d095a4c201d4e
kimsangbin <tkdqls612@naver.com>
2022-06-02 22:10:57 +0900

    global validation exception

--- src/main/java/com/example/exception/controller/ApiController.java
+++ src/main/java/com/example/exception/controller/ApiController.java
@@ -3,23 +3,34 @@ package com.example.exception.controller;
 import com.example.exception.dto.User;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @RestController
 @RequestMapping("/api/user")
+@Validated // 속성 제약조건에 대한 그룹을 만들어 적용시킬 수 있다.
 public class ApiController {
 
     //TODO required : name 의 값이 없어도 실행
     @GetMapping("")
-    public User get(@RequestParam(required = false) String name, @RequestParam(required = false) Integer age){
+    public User get(
+
+            @Size(min = 2)
+            @RequestParam String name,
+
+            @NotNull
+            @Min(1)
+            @RequestParam Integer age){
         User user = new User();
         user.setName(name);
         user.setAge(age);
 
-        int a = 10 + age;
         return user;
 
     }
@@ -31,10 +42,10 @@ public class ApiController {
     }
 
     //TODO ApiController 안에서 선언을 해주게 되면 컨트롤러 안에서 에러를 잡게 된다.
-    @ExceptionHandler(value = MethodArgumentNotValidException.class)
-    public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
-        System.out.println("api controller");
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-    }
+    //@ExceptionHandler(value = MethodArgumentNotValidException.class)
+    //public ResponseEntity methodArgumentNotValidException(MethodArgumentNotValidException e) {
+    //System.out.println("api controller");
+    //return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
+//    }
 
 }
